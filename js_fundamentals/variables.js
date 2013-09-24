d264dc130f4cddcae9e974720815732a569bc173
Michael van Holst <mikevanholst@gmail.com>
2013-09-24 15:13:58 -0400

    completes variables exercises

--- js_fundamentals/variables.js
+++ js_fundamentals/variables.js
@@ -7,7 +7,15 @@
 // Store the following into variables: number of children, partner's name, geographic location, job title.
 // Output your fortune to the screen like so: "You will be a X in Y, and married to Z with N kids."
 
-// EXERCISE: The Age Calculator
+// numberOfChildren = 2;
+// partnersName = "Morgan";
+// geographicLocation = "London, ON";
+// jobTitle = "Teacher";
+
+// "You will be a " + jobTitle + " in " + geographicLocation  + " and married to " + partnersName + " with " + numberOfChildren + " kids.";
+
+
+// EXERCISE: The Age Calculator;
 
 // Forgot how old someone is? Calculate it!
 
@@ -15,6 +23,19 @@
 // Store their birth year in a variable.
 // Calculate their 2 possible ages based on the stored values.
 // Output them to the screen like so: "They are either NN or NN", substituting the values.
+
+// x = 1962;
+// currentYear = 2013;
+// birthYear = x;
+// age1 = currentYear - x;
+// age2 = currentYear - x - 1;
+
+// alert ("They are either " + age1 + " or " + age2 + ".");
+
+
+
+
+
 
 // EXERCISE: The Lifetime Supply Calculator
 
@@ -26,6 +47,16 @@
 // Calculate how many you would eat total for the rest of your life.
 // Output the result to the screen like so: "You will need NN to last you until the ripe old age of X".
 
+// currentAge = 35;
+// maximumAge = 100;
+// desiredDailyQuantity = 2;
+// lifetimeQuantity = desiredDailyQuantity * (maximumAge - currentAge) * 365;
+// alert(lifetimeQuantity);
+
+
+
+
+
 // EXERCISE: The Geometrizer
 
 // Calculate properties of a circle, using the definitions here.
@@ -33,6 +64,16 @@
 // Store a radius into a variable.
 // Calculate the circumference based on the radius, and output "The circumference is NN".
 // Calculate the area based on the radius, and output "The area is NN".
+
+
+// radius = 10;
+// circumference = Number((2 * Math.PI * radius).to_ixed(1));
+// area = Number((Math.PI  * radius * radius).toFixed());
+// alert("The circumference is " + circumference + "units, and the area is " + area + " square units.");
+
+
+
+
 
 // EXERCISE: The Temperature Converter
 
@@ -42,5 +83,16 @@
 // Convert it to fahrenheit and output "NN°C is NN°F".
 // Now store a fahrenheit temperature into a variable.
 // Convert it to celsius and output "NN°F is NN°C."
+
+
+// celsiusTemperature = 100;
+// fahrenheitTemperature = celsiusTemperature * 9/5+ 32;
+// alert(celsiusTemperature +"°C is " + fahrenheitTemperature + "°F");
+
+// fahrenheitTemperature = 212;
+// celsiusTemperature = (fahrenheitTemperature - 32) * 5/9;
+// alert(fahrenheitTemperature + "°F is " + celsiusTemperature +"°C");
+
+
 
 
